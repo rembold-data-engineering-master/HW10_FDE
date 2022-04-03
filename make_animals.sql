abe45c9f20f96ef1903cf09630f481be27e97d83
Jed Rembold <jedediah.r@gmail.com>
2022-04-03 01:57:26 -0700

    Initial commit

---
+++ make_animals.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS animals (
+    name text
+);
+INSERT INTO animals VALUES
+('ant'),
+('ass'),
+('bear'),
+('bull'),
+('cat'),
+('crow'),
+('dog'),
+('eagle'),
+('fox'),
+('frog'),
+('goat'),
+('grasshopper'),
+('hare'),
+('lion'),
+('monkey'),
+('mouse'),
+('owl'),
+('ox'),
+('snake'),
+('wolf');
